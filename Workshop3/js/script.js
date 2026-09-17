9de4a7fd066c4cb42d535bfd7780fa995064aff9
Mette Holmström <meh00055@laurea.fi>
2026-09-17 13:19:00 +0300

    eläintaulukko luotu

--- Workshop3/js/script.js
+++ Workshop3/js/script.js
@@ -1,3 +1,25 @@
 function showMessage() {
     alert("You clicked me!");    
 } 
+function showTable() {
+    const animal = "Tiikeri";
+    const habitat = "Metsä";
+    const diet = "Liha";
+
+    const table = `
+        <table class="display">
+            <tr>
+                <th>Eläin</th>
+                <th>Elinympäristö</th>
+                <th>Ruokavalio</th>
+            </tr>
+            <tr>
+                <td>${animal}</td>
+                <td>${habitat}</td>
+                <td>${diet}</td>
+            </tr>
+        </table>
+    `;
+
+    document.querySelector("#tableContainer").innerHTML = table;
+}
