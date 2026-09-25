1cdd2b412a376b64b8fcd6c120cf5aaeb296b0d9
Mette Holmström <meh00055@laurea.fi>
2026-09-25 08:24:11 +0300

    image kansio luotu

--- WS04_DOM/js/script.js
+++ WS04_DOM/js/script.js
@@ -9,7 +9,42 @@ changeHeadingButton.addEventListener("click", function () {
 // -------------------------------------------------- EXAMPLE 1 ANIMAL TABLE
 // -------------------------------------------------- EXAMPLE 1 ANIMAL TABLE
 
+// Muutetaan otsikon tyyliä
+const changeStyleButton = document.querySelector("#changeStyleButton");
 
+changeStyleButton.addEventListener("click", function () {
+    taskOneHeading.classList.toggle("highlight");
+});
+
+
+// Muutetaan eläintekstiä
+const changeTextButton = document.querySelector("#changeTextButton");
+const animalText = document.querySelector("#animalText");
+
+changeTextButton.addEventListener("click", function () {
+    animalText.textContent = "Elefantit elävät Afrikassa ja Aasiassa.";
+});
+
+// TEHTÄVÄ 2
+
+const animalContent = document.querySelector("#animalContent");
+
+// Luodaan otsikko
+const animalHeading = document.createElement("h3");
+animalHeading.textContent = "Päivän eläin";
+animalHeading.classList.add("animal-heading");
+
+// Luodaan tekstikappale
+const animalParagraph = document.createElement("p");
+animalParagraph.textContent = "Tiikeri on maailman suurin kissaeläin.";
+
+// Luodaan kuva
+const animalImage = document.createElement("img");
+animalImage.src = "images/tiger.png";
+animalImage.alt = "Tiikeri";
+
+// Lisätään elementit sivulle
+animalContent.append(animalHeading, animalParagraph, animalImage);
 
 // -------------------------------------------------- EXAMPLE 3 LISTEN DROPDOWN SELECT
 // -------------------------------------------------- EXAMPLE 3 LISTEN DROPDOWN SELECT
